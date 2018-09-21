ff463feed93cf1c3818a785d891e3826b2cdcb0f
Alexandre Pontes <alexandre.pontes@netdevices.fr>
2018-09-21 14:31:03 +0200

    implementation of the old code

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 
 import NavBar from '../../common/components/basics/NavBar';
 import Banner from '../../common/components/Banner';
+import Services from '../../common/components/Services';
+import About from '../../common/components/About';
+import ServicesNumber from '../../common/components/Services/Number';
+import BannerFlou from '../../common/components/Banner/Flou';
+import Footer from '../../common/components/basics/Footer';
 
 import './styles.scss';
 
@@ -16,6 +21,11 @@ class Home extends React.Component{
       <div>
         <NavBar />
         <Banner />
+        <Services />
+        <About />
+        <ServicesNumber />
+        <BannerFlou />
+        <Footer />
       </div>
     );
   }
