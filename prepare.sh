b6dfcae9dd1ea80696355c7f03353008f85704a2
Alexandre Pontes <alexandre@MacBook-Pro-de-Alexandre.local>
2018-09-26 14:23:11 +0200

    adding prepare.sh file to uptate all project easaly

---
+++ prepare.sh
@@ -0,0 +1,13 @@
+git pull origin master && npm install
+cd src/common/
+git pull origin master && npm install
+clear
+echo " "
+echo "-------------------------------------------------------------------------"
+echo " "
+echo "  Mise à jour effectuée avec succès"
+echo "  Si vous voulez ouvrir l'éditeur 'Atom', executez le script en faisant : "
+echo "  ./prepare.sh && atom ."
+echo " "
+echo "-------------------------------------------------------------------------"
+echo " "
